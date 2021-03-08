1b8ef71af9de616d5ca832cde97db9ebcc1b17a1
chuck-stein <cfgstein@gmail.com>
2021-03-07 21:55:39 -0500

    Removed unused functions in Util.kt

--- app/src/main/java/io/libzy/common/Util.kt
+++ app/src/main/java/io/libzy/common/Util.kt
@@ -1,22 +1,17 @@
 package io.libzy.common
 
-import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Context
-import android.os.Handler
-import android.os.Looper
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.*
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.navigation.NavDeepLinkBuilder
 import com.google.firebase.analytics.ktx.ParametersBuilder
 import io.libzy.R
 import io.libzy.view.MainActivity
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeoutException
 import kotlin.math.roundToInt
 
 // TODO: break this file into specific utils, e.g. AndroidUtil, MathUtil
@@ -47,62 +42,6 @@ val ViewGroup.children: List<View>
         }
         return children
     }
-
-// adapted from https://stackoverflow.com/a/54648758
-fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
-    observe(lifecycleOwner, object : Observer<T> {
-        override fun onChanged(t: T?) {
-            removeObserver(this)
-            if (lifecycleOwner.lifecycle.currentState != Lifecycle.State.DESTROYED) observer.onChanged(t)
-        }
-    })
-}
-
-// adapted from https://github.com/android/architecture-components-samples/blob/master/GithubBrowserSample/app/src/test-common/java/com/android/example/github/util/LiveDataTestUtil.kt
-fun <T> LiveData<T>.getOrAwaitValue(timeoutSeconds: Long = 2): T {
-    var value: T? = null
-    val latch = CountDownLatch(1)
-    val observer = object : Observer<T> {
-        override fun onChanged(newValue: T) {
-            value = newValue
-            latch.countDown()
-            this@getOrAwaitValue.removeObserver(this)
-        }
-    }
-
-    // ensure observeForever runs on main thread, otherwise it will throw exception
-    Handler(Looper.getMainLooper()).post {
-        this.observeForever(observer)
-    }
-
-    if (!latch.await(timeoutSeconds, TimeUnit.SECONDS)) {
-        this.removeObserver(observer)
-        throw TimeoutException("LiveData value was never set.")
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    return value as T
-}
-
-// TODO: get country/state/city abbreviations from library or online?
-val UPPER_CASE_WORDS = setOf(
-    "edm", "idm", "r&b", "opm", "usbm", "uk", "ny", "nl", "us", "nc", "tx", "nz", "dc", "fl", "kc", "okc", "rva", "ecm",
-    "lds", "asmr", "lldm", "stl", "gbvfi", "uk82", "nyhc", "ukhc", "czsk", "vbs", "diy", "bc", "la", "nyc", "sf", "dfw",
-    "png", "atl", "cedm", "ukg", "ebm", "gqom", "nrg", "lgbtq+", "dmv", "mpb"
-)
-
-val LOWER_CASE_WORDS = setOf("n", "y", "and")
-
-@SuppressLint("DefaultLocale")
-fun String.capitalizeAsHeading(): String = toLowerCase().split(" ").joinToString(" ") { word ->
-    word.split("-").joinToString("-") { wordPart ->
-        when (wordPart) {
-            in UPPER_CASE_WORDS -> wordPart.toUpperCase()
-            in LOWER_CASE_WORDS -> wordPart // input already converted to lower case
-            else -> wordPart.capitalize()
-        }
-    }
-}
 
 fun ParametersBuilder.param(key: String, value: Boolean) {
     val valueAsLong: Long = if (value) 1 else 0
