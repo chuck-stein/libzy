7a19e3723135554475a1598a099f7a387f202984
chuck-stein <cfgstein@gmail.com>
2021-04-05 23:25:52 -0400

    Prevent Query class from being minified to fix crash

--- app/src/main/java/io/libzy/model/Query.kt
+++ app/src/main/java/io/libzy/model/Query.kt
@@ -1,10 +1,13 @@
 package io.libzy.model
 
+import androidx.annotation.Keep
+
 /**
  * Represents a user's mood / listening preferences, as a collection of desired album properties.
  * 
  * Null values indicate no preference for the associated property.
  */
+@Keep
 data class Query(
     val familiarity: Familiarity? = null,
     val instrumental: Boolean? = null,
@@ -14,7 +17,7 @@ data class Query(
     val danceability: Float? = null,
     val genres: Set<String>? = null
 ) {
-    enum class Familiarity(val value: String) {
+    @Keep enum class Familiarity(val value: String) {
         CURRENT_FAVORITE("current favorite"),
         RELIABLE_CLASSIC("reliable classic"),
         UNDERAPPRECIATED_GEM("underappreciated gem")
