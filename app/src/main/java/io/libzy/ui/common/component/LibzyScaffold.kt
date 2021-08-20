87ef9cd362bf413b9f40d7a524b6adff78cbdea5
chuck-stein <cfgstein@gmail.com>
2021-08-19 22:17:13 -0400

    Use `accompanist-insets-ui` for `Scaffold` and `TopAppBar`

--- app/src/main/java/io/libzy/ui/common/component/LibzyScaffold.kt
+++ app/src/main/java/io/libzy/ui/common/component/LibzyScaffold.kt
@@ -2,24 +2,28 @@ package io.libzy.ui.common.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Snackbar
 import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.navigationBarsHeight
 import com.google.accompanist.insets.navigationBarsPadding
-import com.google.accompanist.insets.statusBarsPadding
+import com.google.accompanist.insets.rememberInsetsPaddingValues
+import com.google.accompanist.insets.ui.Scaffold
+import com.google.accompanist.insets.ui.TopAppBar
 import io.libzy.ui.BackgroundGradient
 import io.libzy.ui.LibzyContent
 
@@ -46,14 +50,20 @@ fun LibzyScaffold(
                 title = title,
                 navigationIcon = navigationIcon,
                 backgroundColor = Color.Transparent,
-                modifier = Modifier.statusBarsPadding(),
-                elevation = 0.dp
+                elevation = 0.dp,
+                contentPadding = rememberInsetsPaddingValues(
+                    insets = LocalWindowInsets.current.statusBars,
+                    applyBottom = false,
+                )
             )
+        },
+        bottomBar = {
+            Spacer(Modifier.navigationBarsHeight().fillMaxWidth())
         },
         snackbarHost = { LibzySnackbarHost(it) },
         backgroundColor = Color.Transparent
-    ) {
-        Box(modifier = Modifier.navigationBarsPadding().fillMaxSize(), content = content)
+    ) { contentPadding ->
+        Box(Modifier.padding(contentPadding).fillMaxSize(), content = content)
     }
 }
 
