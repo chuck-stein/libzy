d1145d75c94b84c3521babf1b9b7703757407a8b
chuck-stein <cfgstein@gmail.com>
2021-08-22 22:21:35 -0400

    Add a text-based key to `Chip`

--- app/src/main/java/io/libzy/ui/common/component/Chip.kt
+++ app/src/main/java/io/libzy/ui/common/component/Chip.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,19 +24,21 @@ fun Chip(selected: Boolean, text: String, onClick: () -> Unit) {
     val backgroundAlpha = if (selected) 0.6f else 0.1f
     val borderStroke = if (selected) null else BorderStroke(1.dp, MaterialTheme.colors.primary)
 
-    Surface(
-        color = MaterialTheme.colors.primary.copy(alpha = backgroundAlpha),
-        shape = RoundedCornerShape(18.dp),
-        border = borderStroke
-    ) {
-        Box(modifier = Modifier.clickable(onClick = onClick)) {
-            Text(
-                text,
-                style = MaterialTheme.typography.body2,
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1,
-                modifier = Modifier.padding(8.dp)
-            )
+    key(text) {
+        Surface(
+            color = MaterialTheme.colors.primary.copy(alpha = backgroundAlpha),
+            shape = RoundedCornerShape(18.dp),
+            border = borderStroke
+        ) {
+            Box(modifier = Modifier.clickable(onClick = onClick)) {
+                Text(
+                    text,
+                    style = MaterialTheme.typography.body2,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
         }
     }
 }
